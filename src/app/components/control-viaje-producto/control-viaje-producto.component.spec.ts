9b8319c3bef5ede4ec1387ecd4088dabfdf755d9
samuel <samueljcp@gmail.com>
2024-10-24 14:12:09 -0500

    new

--- src/app/components/control-viaje-producto/control-viaje-producto.component.spec.ts
+++ src/app/components/control-viaje-producto/control-viaje-producto.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ControlViajeProductoComponent } from './control-viaje-producto.component';
+import { controlViajeProductoComponent} from './control-viaje-producto.component';
 
 describe('ControlViajeProductoComponent', () => {
-  let component: ControlViajeProductoComponent;
-  let fixture: ComponentFixture<ControlViajeProductoComponent>;
+  let component: controlViajeProductoComponent;
+  let fixture: ComponentFixture<controlViajeProductoComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ControlViajeProductoComponent]
+      imports: [controlViajeProductoComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ControlViajeProductoComponent);
+    fixture = TestBed.createComponent(controlViajeProductoComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
--- src/app/components/control-viaje-producto/controlViajeProducto.routes.ts
+++ src/app/components/control-viaje-producto/controlViajeProducto.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { ControlViajeProductoComponent } from './control-viaje-producto.component';
+import { controlViajeProductoComponent } from './control-viaje-producto.component';
 
 export const CONTROLVIAJEPRODUCTO_ROUTES: Routes = [
   {
     path: '',
-    component: ControlViajeProductoComponent,
+    component: controlViajeProductoComponent,
   },
 ];
