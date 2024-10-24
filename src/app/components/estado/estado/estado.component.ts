78adde5ef1014ba72d0d1bb1d25dd2813d94397e
Diego <alejandrodiegoqc@gmail.com>
2024-10-23 21:04:38 -0500

    add estado frontend

--- src/app/components/estado/estado/estado.component.ts
+++ src/app/components/estado/estado/estado.component.ts
@@ -1,30 +1,81 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
-import { EstadoService } from '../service/estado.service';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { Estado } from '../models/estado-model';
+import { EstadoService } from '../service/estado.service';
 
 @Component({
   selector: 'app-estado',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule, FormsModule],
   templateUrl: './estado.component.html',
   styleUrls: ['./estado.component.css'],
   providers: [EstadoService],
 })
-export class EstadoComponent {
+export class EstadoComponent implements OnInit {
   estados: Estado[] = [];
+  selectedEstado: Estado | null = null;
 
-  constructor(private userService: EstadoService) {}
+  constructor(private estadoService: EstadoService) {}
 
   ngOnInit(): void {
-    this.userService.getUsers().subscribe(
-      (data: Estado[]) => {
+    this.getEstados();
+  }
+
+  getEstados(): void {
+    this.estadoService.getEstado().subscribe(
+      (data) => {
         this.estados = data;
       },
-      (error: any) => {
-        console.error('Error al obtener los usuarios:', error);
+      (error) => {
+        console.error('Error al obtener los estados:', error);
       }
     );
+  }
+
+  addEstado(estado: Estado): void {
+    this.estadoService.createEstado(estado).subscribe(
+      (newEstado) => {
+        this.estados.push(newEstado);
+      },
+      (error) => {
+        console.error('Error al agregar el estado:', error);
+      }
+    );
+  }
+
+  updateConductor(estado: Estado): void {
+    this.estadoService.updateEstado(estado.id, estado).subscribe(
+      () => {
+        const index = this.estados.findIndex((c) => c.id === estado.id);
+        if (index !== -1) {
+          this.estados[index] = estado;
+        }
+      },
+      (error) => {
+        console.error('Error al actualizar el estado:', error);
+      }
+    );
+  }
+
+  deleteEstado(id: number): void {
+    this.estadoService.deleteEstado(id).subscribe(
+      () => {
+        this.estados = this.estados.filter((c) => c.id !== id);
+      },
+      (error) => {
+        console.error('Error al eliminar el estado:', error);
+      }
+    );
+  }
+
+  selectEstado(estado: Estado): void {
+    this.selectedEstado = estado;
+  }
+
+  clearSelection(): void {
+    this.selectedEstado = null;
   }
 }
--- src/app/components/estado/service/estado.service.ts
+++ src/app/components/estado/service/estado.service.ts
@@ -9,10 +9,24 @@ import { Observable } from 'rxjs';
 export class EstadoService {
   private apiUrl = 'https://localhost:5000/api/estado';
   
-
   constructor(private http: HttpClient) {}
 
-  getUsers(): Observable<Estado[]> {
+  getEstado(): Observable<Estado[]> {
     return this.http.get<Estado[]>(this.apiUrl);
+  }
+  getEstadoById(id: number): Observable<Estado> {
+    return this.http.get<Estado>(`${this.apiUrl}/${id}`);
+  }
+
+  createEstado(estado: Estado): Observable<Estado> {
+    return this.http.post<Estado>(this.apiUrl, estado);
+  }
+
+  updateEstado(id: number, estado: Estado): Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${id}`, estado);
+  }
+
+  deleteEstado(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
